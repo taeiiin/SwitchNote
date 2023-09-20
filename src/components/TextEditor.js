165bad2fa378da01ec7add5979ea135c2694ce34
1222pgy <1222pgy@naver.com>
2023-09-21 00:30:57 +0900

    230921 gayeon
    - Workspace 페이지에서 프로젝트 목록 프론트 수정
    - Workspace 페이지에서 프로젝트 요소 하나 클릭하면 TextEditor 페이지로 넘어가도록 함
    - 메인 페이지에서 Script to PPT 클릭하면 TextEditor 페이지로 넘어가도록 함

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,21 +1,35 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import {useParams} from 'react-router-dom'
 import refreshIcon from './images/refreshIcon.png';
 import copyIcon from './images/copyIcon.png';
 import MyButton from './MyButton.js';
 import MyTextarea from './MyTextarea.js';
 
-function TextEditor() {
-  const [text, setText] = useState('');
+function TextEditor({ getProjectById }) {
+  const [projectTitle, setProjectTitle] = useState('');
+  const [project, setProject] = useState(null); // 프로젝트 데이터를 저장할 상태 추가
   const textAreaRef = useRef(null);
+  const { projectId } = useParams();
+
+  useEffect(() => {
+    if (projectId) {
+      const projectData = getProjectById(projectId);
+      if (projectData && textAreaRef.current) {
+        textAreaRef.current.value = projectData.content;
+        setProject(projectData); // 프로젝트 데이터 저장
+        setProjectTitle(projectData.title); // 프로젝트 제목 저장
+      }
+    }
+  }, [projectId]);
   
   const handleTextChange = (e) => {
-    setText(e.target.value);
+    setProject({...project, content: e.target.value});
   };
-
+  
   const handleClearText = () => {
     if(textAreaRef.current) {
       textAreaRef.current.value = '';
-      setText('');
+      setProject({...project, content: ''});
     }
   };
 
@@ -39,10 +53,12 @@ function TextEditor() {
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
         <label htmlFor="fileName"></label>
           <MyTextarea
-            placeholder={'파일명을 입력하세요'}
+            placeholder={project ? project.title : '파일명을 입력하세요'} // 제목이 있으면 그걸 보여주고 없으면 플레이스홀더 텍스트 보여주기
             type={"title"}
+            value={project ? project.title : ''}
+            onChange={(e) => setProject({...project, title: e.target.value})}
           />
-
+  
         <label htmlFor="description"></label>
           <MyTextarea
             placeholder={'부가설명을 입력하세요'}
@@ -64,8 +80,8 @@ function TextEditor() {
           ref={textAreaRef}
           id="text"
           rows="10"
-          value={text}
-          onChange={handleTextChange}
+          value={project ? project.content : ''} // 프로젝트 내용이 있으면 그걸 보여주고 없으면 빈 문자열 보여주기
+          onChange={(e) => setProject({...project, content: e.target.value})} // 내용 변경 시 프로젝트 데이터도 함께 업데이트하기
         />
       </div>
 
--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -2,17 +2,11 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Workspace.css';
 
-// 여기에서 projects를 props로서 받습니다.
 function Workspace({ projects }) {
   const [selected, setSelected] = useState('ppt');
+  const [menuOpen, setMenuOpen] = useState(null);
   const navigate = useNavigate();
 
-  const handleClick = (type) => {
-    setSelected(type);
-  };
-
-  /*PPT 파일과 Script 파일 각각 수정 페이지가 다르므로 type도 함께 전달하는 것으로 수정함*/
-  /*Workspace에 저장되는 각 파일의 projectId에 따라 수정 페이지가 나오도록 하였음*/
   const handleProjectClick = (type, projectId) => {
     if(type === 'ppt') {
       navigate(`/PPTEditor/${projectId}`);
@@ -20,22 +14,31 @@ function Workspace({ projects }) {
     else if (type === 'script') {
       navigate(`/TextEditor/${projectId}`);
     }
+  };  
+
+  // 더보기 메뉴 토글
+  const toggleMenu = (id) => {
+    setMenuOpen(prevId => prevId === id ? null : id);
   };
+
+  const handleEditProject = (project) => {/* 수정 기능*/};
+  const handleDeleteProject = (id) => {/* 삭제 기능*/};
+  const handleDownloadProject = (project) => {/* 다운로드 기능*/};
 
   return (
     <div className="workspace">
       <div className="project-container">
         <button
           className={`project-btn ppt ${selected === 'ppt' ? 'active' : ''}`}
-          onClick={() => handleClick('ppt')}
+          onClick={() => setSelected('ppt')}
         >
           PPT
         </button>
         <button
           className={`project-btn script ${selected === 'script' ? 'active' : ''}`}
-          onClick={() => handleClick('script')}
+          onClick={() => setSelected('script')}
         >
-          Script
+        Script 
         </button>
       </div>
       {/* 선택된 버튼에 따라 배경색상 변경 */}
@@ -46,14 +49,21 @@ function Workspace({ projects }) {
         }}
       >
         {projects.filter((project) => project.type === selected).map((project) =>
-            (
-              <div key={project.id} 
-                   className={`project thumbnail`} 
-                   onClick={() => handleProjectClick(project.type, project.id)}>
-                <img src={project.thumbnail} alt={`${project.title} thumbnail`} />
-                <span>{project.title}</span>
+          (
+            <div key={project.id} 
+                className={`project`} 
+                onClick={() => handleProjectClick(project.type || 'script', project.id)}>
+              <div className="title">
+                <span>
+                  {project.title.length > 10 ? project.title.slice(0,10) + '...' : project.title}
+                </span>
               </div>
-            ))}
+              <div className="thumbnail">
+                <img src={project.thumbnail} alt={`${project.title} thumbnail`} />
+              </div>
+            </div>
+          ))}
+
       </div>
     </div>
   );
