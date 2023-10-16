113b3676acd2442aba8449bbb253f2bad5043fa0
Taein <taein.k0103@gmail.com>
2023-10-16 23:36:38 +0900

    #3 Back 재업로드

---
+++ src/main/java/com/example/switchnote_back/SwitchnoteBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.switchnote_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchnoteBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwitchnoteBackApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/switchnote_back/domain/Agree.java
@@ -0,0 +1,5 @@
+package com.example.switchnote_back.domain;
+
+public enum Agree {
+    N,Y;
+}
