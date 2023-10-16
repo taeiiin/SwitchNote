c06eefb406b37e0ee225d686e76e872ff823f411
Taein <taein.k0103@gmail.com>
2023-10-17 00:14:00 +0900

    #9 Directory 정리, 파일 초기화

--- src/main/java/com/example/switchnote_back/auth/oauth/OAuth2UserInfo.java
+++
@@ -1,8 +0,0 @@
-package com.example.switchnote_back.auth.oauth;
-
-public interface OAuth2UserInfo {
-    String getProviderId();
-    String getProvider();
-    String getEmail();
-    String getName();
-}
--- src/main/java/com/example/switchnote_back/domain/Agree.java
+++
@@ -1,5 +0,0 @@
-package com.example.switchnote_back.domain;
-
-public enum Agree {
-    N,Y;
-}
